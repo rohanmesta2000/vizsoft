69e49b19a30e9f192aff65af8d4c7e30f1d85428
rohanmesta2000 <rohanmesta.xworkz@gmail.com>
2025-01-30 21:10:20 +0530

    Day 2

---
+++ 100 Days of Coding Dart and Flutter/variables.dart
@@ -0,0 +1,16 @@
+void main() {
+ 
+  int age = 25;
+  double height = 5.9;
+  String name = "Rohan";
+  bool isStudent = true;
+  List<int> scores = [85, 90, 78, 92];
+  List<String> names=["rohan","rahul","rohit","chethan"];
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height of the student is : $height");
+  print("Is Student: $isStudent");
+  print("Scores: $scores");
+  print("Names: $names");
+}
