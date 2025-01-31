4aaad8733c66416b5889eea3fb7efd3d4034a532
rohanmesta2000 <139941311+rohanmesta2000@users.noreply.github.com>
2025-01-31 22:19:24 +0530

    Add files via upload
    day 2 task

---
+++ 100 Days of Coding Dart and Flutter/variables.dart
@@ -0,0 +1,19 @@
+void main() {
+ 
+  int age = 25;
+  double height = 5.9;
+  String name = "Rohan";
+  bool isStudent = true;
+  List<int> scores = [85, 90, 78, 92];
+  List<String> names=["rohan","rahul","rohit","chethan"];
+  final weight=50;
+  
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height of the student is : $height");
+  print("Is Student: $isStudent");
+  print("Scores: $scores");
+  print("Names: $names");
+  print("weight: $weight");
+}
